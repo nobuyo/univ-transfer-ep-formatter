f25945d2fbd26778e72c6d925c7a349824be0696
nobuyo <world_fantasy_2017@live.jp>
2017-10-25 12:44:30 +0900

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,15 @@
 Rails.application.routes.draw do
 
+  root 'application#index'
+
   resources :admins, only: [:index, :edit, :show, :update]
   get    'login'    => 'sessions#new'
   post   'login'    => 'sessions#create'
   delete 'logout'   => 'sessions#destroy'
 
-  resources :admins
-  root 'application#index'
-  # resources :books
-  # resources :subjects
-  # resources :univs
   resources :episodes do
     collection do
       get 'dump'
     end
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
